e0e150dd4b91a9d06e4d41afff4e272cac3b0158
Ricardo Lopes <mail@ricardolopes.net>
2011-11-14 20:42:00 +0000

    Trying OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["name"]
-      logger.info auth["name"]
-      logger.info auth["user_info"]
+      user.name = auth["user_info"]
     end
   end
 end
