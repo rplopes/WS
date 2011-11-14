c7d98c32e305683a16d39ae6a32c3794ea5b5686
Ricardo Lopes <mail@ricardolopes.net>
2011-11-14 17:01:26 +0000

    Deleted Koala project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ws::Application.config.secret_token = 'f6642c3713e8908124b661f04fb5c667f7c603d562ae21bba14647a4da6080f615b692f90151b62a6dd80644d61b05631f02819656572e97740cb1e0d831f83d'
