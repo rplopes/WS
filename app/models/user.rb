e6557f0be880596e7934e8934e6b4da04357e238
Ricardo Lopes <mail@ricardolopes.net>
2011-11-14 21:42:13 +0000

    Trying OmniAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       auth.each { |key, value| logger.info "#{key}: #{value}" }
+      auth['info'].each { |key, value| logger.info "#{key}: #{value}" }
       user.provider = auth['provider']
       user.uid = auth['uid']
-      user.name = auth['name']
+      user.name = auth['info']['name']
     end
   end
 end
