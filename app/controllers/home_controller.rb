0fedfa9ebe319eb781143686f5cad8fee70d2d00
Ricardo Lopes <mail@ricardolopes.net>
2011-11-14 22:05:39 +0000

    Different views for signed and unsigned users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      user
+    end
+  end
+  
+  private
+  
+  def user
+    @user = FbGraph::User.new('me', :access_token => session[:omniauth]['credentials']['token'])
+    @user.fetch
   end
 end
