61a87acea97b8e17e623a333756d0a7296ac3da5
Ricardo Lopes <mail@ricardolopes.net>
2011-11-17 15:14:28 +0000

    Testing articles fetch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 require 'rss'
+require 'rexml/document'
 
 class HomeController < ApplicationController
 
@@ -10,29 +11,33 @@ class HomeController < ApplicationController
   
   def latest_news
     @titles = []
-    file = File.new('articles.xml', 'w')
-
-    rss_urls = ["http://feeds.ign.com/ignfeeds/movies/", "http://feeds.ign.com/ignfeeds/tv/", "http://www.comingsoon.net/rss-database-20.php", "http://www.comingsoon.net/trailers/rss-trailers-20.php", "http://www.comingsoon.net/news/rss-main-30.php", "http://news.yahoo.com/rss/movies", "http://feeds2.feedburner.com/NewsInFilm", "http://feeds.feedburner.com/totalfilm/news"]
-
-    xml = Builder::XmlMarkup.new( :target => file, :indent => 2)
-    xml.instruct! :xml, :encoding => "UTF-8"
-
-    xml.articles do
-      rss_urls.each do |url|
-        rss = RSS::Parser.parse(open(url).read, false)
-        rss.items.each do |item|
-          @titles << item.title
-          xml.article do |n|
-            n.title item.title
-            n.date item.date
-            n.description item.description
-            n.link item.link
-          end
-        end
-      end
+    doc = REXML::Document.new File.new('articles.xml')
+    doc.elements.each("articles/article") do |article|
+      @titles << article.elements["title"].text
     end
+#    file = File.new('articles.xml', 'w')
 
-    file.close
+#    rss_urls = ["http://feeds.ign.com/ignfeeds/movies/", "http://feeds.ign.com/ignfeeds/tv/", "http://www.comingsoon.net/rss-database-20.php", "http://www.comingsoon.net/trailers/rss-trailers-20.php", "http://www.comingsoon.net/news/rss-main-30.php", "http://news.yahoo.com/rss/movies", "http://feeds2.feedburner.com/NewsInFilm", "http://feeds.feedburner.com/totalfilm/news"]
+
+#    xml = Builder::XmlMarkup.new( :target => file, :indent => 2)
+#    xml.instruct! :xml, :encoding => "UTF-8"
+
+#    xml.articles do
+#      rss_urls.each do |url|
+#        rss = RSS::Parser.parse(open(url).read, false)
+#        rss.items.each do |item|
+#          @titles << item.title
+#          xml.article do |n|
+#            n.title item.title
+#            n.date item.date
+#            n.description item.description
+#            n.link item.link
+#          end
+#        end
+#      end
+#    end
+
+#    file.close
   end
   
   def browse
