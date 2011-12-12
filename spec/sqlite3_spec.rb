3252c661a3a58915e029934df00a5dff17788e91
Rui Chicória <rchicoria@gmail.com>
2011-12-12 17:17:53 +0000

    .

---
+++ spec/sqlite3_spec.rb
@@ -0,0 +1,23 @@
+# spec/sqlite3.spec
+$:.unshift File.dirname(__FILE__) + "/../lib/"
+
+require 'rdf'
+require 'rdf/do'
+require 'rdf/spec/repository'
+require 'do_sqlite3'
+
+describe RDF::DataObjects::Repository do
+  context "The SQLite adapter" do
+    before :each do
+      @repository = RDF::DataObjects::Repository.new "sqlite3::memory:"
+    end
+
+    after :each do
+      # DataObjects pools connections, and only allows 8 at once.  We have
+      # more than 60 tests.
+      DataObjects::Sqlite3::Connection.__pools.clear
+    end
+
+    it_should_behave_like RDF_Repository
+  end
+end
