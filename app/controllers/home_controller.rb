2546453f6dac707fe72829e0e09a22288ee6a3c6
Ricardo Lopes <mail@ricardolopes.net>
2011-12-13 07:46:46 +0000

    First steps to semantic search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,6 +33,53 @@ class HomeController < ApplicationController
   def search
     @search = params[:search]
     @page_title = "Results for \"#{@search}\""
+    @articles = []
+    
+    search = @search.gsub('"', '\"')
+    @it_is = nil
+    
+    # Is a movie?
+    q = "SELECT *
+         WHERE { ?x <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#hasTitle> \"#{search}\" .
+                 ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#Movie> }"
+    results = query(q)
+    @it_is = "the movie" if results.size > 0
+    
+    # Is a TV show?
+    unless @it_is
+      q = "SELECT *
+           WHERE { ?x <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#hasTitle> \"#{search}\" .
+                   ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#TVShow> }"
+      results = query(q)
+      @it_is = "the TV show" if results.size > 0
+    end
+    
+    # Is an actor?
+    unless @it_is
+      q = "SELECT *
+           WHERE { ?x <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#hasName> \"#{search}\" .
+                   ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#Actor> }"
+      results = query(q)
+      @it_is = "the actor" if results.size > 0
+    end
+    
+    # Is a director?
+    unless @it_is
+      q = "SELECT *
+           WHERE { ?x <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#hasName> \"#{search}\" .
+                   ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#Director> }"
+      results = query(q)
+      @it_is = "the movies director" if results.size > 0
+    end
+    
+    # Is a creator?
+    unless @it_is
+      q = "SELECT *
+           WHERE { ?x <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#hasName> \"#{search}\" .
+                   ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.semanticweb.org/ontologies/2011/10/moviesandtv.owl#Creator> }"
+      results = query(q)
+      @it_is = "the TV shows creator" if results.size > 0
+    end
   end
   
   def suggestions
