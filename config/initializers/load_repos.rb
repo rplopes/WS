43443d7659d81cf7f0b15dada15f3cc5ed38c236
Rui Chicória <rchicoria@gmail.com>
2012-01-07 21:10:28 +0000

    .

--- config/initializers/load_repos.rb
+++ config/initializers/load_repos.rb
@@ -5,3 +5,20 @@ else
 	Article.rebuild_index
 end
 REPOSITORY = RDF::Repository.new << TRIPLE_STORE
+
+def repeat_every(interval)
+  Thread.new do
+    loop do
+      start_time = Time.now
+      yield
+      elapsed = Time.now - start_time
+      sleep([interval - elapsed, 0].max)
+    end
+  end
+end
+
+thread = repeat_every(5) do
+  puts Time.now.to_i
+end  
+puts "Doing other stuff..."
+thread.join
