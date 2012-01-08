ed8389f733d235a758597ae36094160221bd39eb
Rui Chicória <rchicoria@gmail.com>
2012-01-08 22:48:07 +0000

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,12 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    session[:omniauth] = auth
-    redirect_to root_url, :notice => "Signed in!"
+    begin
+      session[:omniauth] = auth
+      redirect_to root_url, :notice => "Signed in!"
+    rescue
+      destroy
+    end
   end
 
   def destroy
